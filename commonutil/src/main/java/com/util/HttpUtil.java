1b295be6769d308b02f53cff0901d44133fbbe02
sunyiban <sunyb@huazhenit.com>
2018-09-01 15:39:53 +0800

    sample

--- commonutil/src/main/java/com/util/HttpUtil.java
+++ commonutil/src/main/java/com/util/HttpUtil.java
@@ -1,5 +1,6 @@
 package com.util;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.PostMethod;
 
@@ -71,6 +72,61 @@ public class HttpUtil {
             return src.endsWith(trimString) ? src.substring(0, src.lastIndexOf(trimString)) : src;
         }
     }
+
+    /**
+     * 根据URL获取文件流并返回base64后的字符串
+     *
+     * @author sunyiban
+     * @date 2018/9/1 15:32
+     * @param urlPath
+     * @return java.lang.String
+     */
+    public static String getUrlResource(String urlPath) {
+		String result = null;
+		InputStream inputStream = null;
+		try {
+			// 这是一些测试用的地址，在使用这个方法的时候遇到一个问题，就是读取不到文件流的内容
+//			URL url = new URL("https://file.52touzi.cn/file/show/5b97c40ee4b0d5c4d6a8cab3.png");
+//			URL url = new URL("https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png");
+//			URL url = new URL("http://thyrsi.com/t6/369/1536675023x-1566680355.png");
+			URL url = new URL(urlPath);
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+			//设置超时间为3秒
+			conn.setConnectTimeout(3*1000);
+			//防止屏蔽程序抓取而返回403错误
+			conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+
+			//得到输入流
+			inputStream =  conn.getInputStream();
+
+			// 原因就是inputStream.available()这个方法，这个方法默认返回值为0，如果没有重写的话，他的返回值会受到你网络传输的影响，
+			// 比如一条100Byte的数据传输过来，inputStream.available()的值可能是100 也可能是50 或者0，这时候直接使用
+			// inputStream.available()来作为读取对象的字节长度就会出问题
+			// 以下这个方法可以解决问题，但不是最好的方案，所以使用这个方法时要是否慎重
+			int count = 0;
+			while (count == 0) {
+				count = inputStream.available();
+			}
+
+			byte[] imageBytes = new byte[inputStream.available()];
+			inputStream.read(imageBytes);
+			result = Base64.encodeBase64String(imageBytes);
+
+			System.out.println(result);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
 
 	public static void main(String[] args) throws Exception{
 		String result = post("https://baike.baidu.com/item/ssl/320778?fr=aladdin", null);
--- commonutil/src/main/java/com/util/ZipUtil.java
+++ commonutil/src/main/java/com/util/ZipUtil.java
@@ -33,10 +33,10 @@ public class ZipUtil {
 			File[] files = file.listFiles();
 			for (File f : files) {
 				if (f.isDirectory()) {
-					System.out.println(f.getName());
+					System.out.println(f.getAbsolutePath() + ":" + f.getPath());
 					listFileDirecotry(f);
 				} else {
-					System.out.println(f.getName());
+					System.out.println(f.getAbsolutePath() + ":" + f.getPath());
 				}
 			}
 		}
@@ -58,6 +58,10 @@ public class ZipUtil {
 	public static File zipSingleFile(File file, String fileName) {
 		if (file == null || !file.exists()) {
 			return null;
+		}
+
+		if (file.isDirectory()) {
+			throw new RuntimeException("不是一个文件");
 		}
 
 		File zipedFile = null;
@@ -156,16 +160,83 @@ public class ZipUtil {
 		return zipedFile;
 	}
 
+	public static File zipFile(File file, String fileName) throws Exception {
+		if (file == null) {
+			return null;
+		}
+
+		if (fileName == null || fileName.length() < 1 || !fileName.endsWith(".zip")) {
+			fileName = file.getAbsolutePath().substring(0, file.getAbsolutePath().lastIndexOf(".")) + ".zip";
+		}
+
+		if (!file.isDirectory()) {
+			return zipSingleFile(file, fileName);
+		}
+
+		File zipFile = new File(fileName);
+		ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile));
+		writeToZipStream(file, zos, "");
+
+		return zipFile;
+	}
+
+	/**
+	 * 这个方法将文件流写入zip
+	 *
+	 * @author sunyiban
+	 * @date 2018/9/7 11:27
+	 * @param file 需要写入的文件
+	 * @param zos zip流
+	 * @param fileName 文件路径绝对路径
+	 * @return void
+	 */
+	private static void writeToZipStream(File file, ZipOutputStream zos, String fileName) {
+		BufferedInputStream bis = null;
+
+		try {
+			File[] files = file.listFiles();
+			for (File f : files) {
+				if (f.isDirectory()) {
+					writeToZipStream(f, zos, "");
+				} else {
+					bis = new BufferedInputStream(new FileInputStream(file));
+					ZipEntry zipEntry = new ZipEntry(file.getAbsolutePath());
+					byte[] readByte = new byte[1024];
+					int len;
+					while((len = bis.read(readByte)) > 0) {
+						zos.write(readByte, 0, len);
+					}
+				}
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (bis != null) {
+				try {
+					bis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
 
 	public static void main(String[] args) throws Exception {
-//		listFileDirecotry(new File("E:/workspace"));
-		zipSingleFile(new File("E:/t.png"), "E:/t.zip");
+		listFileDirecotry(new File("E:/workspace"));
+//		zipSingleFile(new File("E:/t.png"), "E:/t.zip");
 
 		/*List<File> listFile = new ArrayList<File>();
 		listFile.add(new File("E:/test.pdf"));
 		listFile.add(new File("E:/test1.pdf"));
 		listFile.add(new File("E:/test2.pdf"));
 		zipFileList(listFile, "E:/test.zip");*/
+
+//		zipFile(new File("E:/workspace"), "E:/ttt.zip");
 	}
 
 }
